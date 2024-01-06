b7221493e438ba9777fe4bb36e167fe90b2a0720
Shinichi Nomura <shinichi.nomura@gmail.com>
2024-01-06 22:14:52 +0900

    レシピページのコンテンツを取得できないケースに対応

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -18,9 +18,11 @@ export default function Mypage() {
   const [recipeTitle, setRecipeTitle] = useState<string>("")
   const [recipeImageUrl, setRecipeImageUrl] = useState<string>("")
 
+  const [previewFailed, setPreviewFailed] = useState<boolean>(false)
+
   const [recipes, setRecipes] = useState<RecipeResponse[]>([])
 
-  useEffect(() => {
+  const loadRecipes = () => {
     fetch(`http://localhost:3000/secure/recipes`, {
       method: "GET",
       headers: {
@@ -37,6 +39,10 @@ export default function Mypage() {
           setRecipes(data.recipes)
         }
       })
+  }
+
+  useEffect(() => {
+    loadRecipes()
   }, [])
 
   const recipeUrlChanged = (value: string) => {
@@ -58,6 +64,7 @@ export default function Mypage() {
       })
       .then((data) => {
         if (data.status === 'error') {
+          setPreviewFailed(true)
         }
         else {
           setRecipeTitle(data.og_title)
@@ -92,6 +99,8 @@ export default function Mypage() {
           setRecipeUrl("")
           setRecipeTitle("")
           setRecipeImageUrl("")
+
+          loadRecipes()
         }
       })
   }
@@ -102,8 +111,8 @@ export default function Mypage() {
         <h2 className="text-2xl text-center">マイページ</h2>
       </div>
       <div className="mx-auto w-full max-w-sm mt-8">
-        <label htmlFor="recipeUrl" className="block">レシピURL</label>
-        <input type="recipeUrl"
+        <label htmlFor="recipeUrl" className="block text-sm text-gray-500">レシピURL</label>
+        <input type="text"
                required
                name="recipeUrl"
                id="recipeUrl"
@@ -113,19 +122,41 @@ export default function Mypage() {
                className="block w-full rounded-md ring-1 ring-inset ring-gray-300 p-2 mt-1"
         />
       </div>
-      <div className="mx-auto w-full max-w-sm mt-8">
-        <div className="flex">
-          <div className="flex-none">
-            <img src={recipeImageUrl} className="w-36 object-contain"/>
-          </div>
-          <div className="flex-grow ml-4">
-            <div className="text-md">{recipeTitle}</div>
-          </div>
-        </div>
+      <div className="mx-auto w-full max-w-sm">
+        {
+          previewFailed ? (
+            <div>
+              <div className="rounded-md border border-solid border-red-400 p-2 text-red-400 mt-2">
+                <div className="text-center text-md">指定されたURLの情報を取得できませんでした</div>
+              </div>
+              <div className="mt-4">
+                <label htmlFor="recipeUrl" className="block text-sm text-gray-500">レシピ名</label>
+                <input type="text"
+                       name="recipeTitle"
+                       id="recipeTitle"
+                       value={recipeTitle}
+                       onChange={(event) => setRecipeTitle(event.target.value)}
+                       className="block w-full rounded-md ring-1 ring-inset ring-gray-300 p-2 mt-1"
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="flex mt-8">
+              <div className="flex-none">
+                <img src={recipeImageUrl} className="w-36 object-contain"/>
+              </div>
+              <div className="flex-grow ml-4">
+                <div className="text-md">{recipeTitle}</div>
+              </div>
+            </div>
+          )
+        }
         <div className="mt-6">
           <button onClick={(event) => addRecipe(event)}
-                  className="w-full rounded-md bg-emerald-600 hover:bg-emerald-500 py-2 text-white"
-          >レシピを登録</button>
+                  disabled={!recipeUrl || !recipeTitle}
+                  className="w-full rounded-md bg-emerald-600 hover:bg-emerald-500 py-2 text-white disabled:opacity-50 disabled:cursor-not-allowed"
+          >レシピを登録
+          </button>
         </div>
       </div>
       <div className="mx-auto w-full max-w-sm mt-8">
@@ -133,9 +164,19 @@ export default function Mypage() {
           {recipes.map((recipe) => {
             return (
               <li key={recipe.id} className="flex mb-4">
-                <div className="flex-none w-36">
-                  {recipe.image_url && <img src={recipe.image_url} alt={recipe.title} className="w-36 object-contain rounded-md"/>}
-                </div>
+                {
+                  recipe.image_url ? (
+                    <div className="flex-none w-36">
+                      <img src={recipe.image_url} alt={recipe.title} className="w-36 object-contain rounded-md"/>
+                    </div>
+                  ) : (
+                    <div className="flex-none w-36">
+                      <div className="w-36 h-16 rounded-md border border-solid border-gray-300 flex justify-center items-center">
+                        <div className="text-gray-300 text-sm">No Image.</div>
+                      </div>
+                    </div>
+                  )
+                }
                 <div className="flex-grow ml-2">
                   <span className="text-sm"><a href={recipe.url} target="_blank">{recipe.title}</a></span>
                 </div>
